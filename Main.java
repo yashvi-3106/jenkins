860b445d60bab7fc5bf9f26dfc38c7c299fa9b6e
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2026-04-22 15:35:56 +0530

    first commit

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello from vscode");
-
+        System.out.println("2nd commit should be auto");
     }
 }
